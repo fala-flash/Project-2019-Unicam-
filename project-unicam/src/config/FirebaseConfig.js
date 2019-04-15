079baf80f62ec2e7886fa70fa018a4041b0933ba
fala-flash <andrea0falaschini@gmail.com>
2019-04-15 17:39:26 +0200

    firebase setup and deploy

---
+++ project-unicam/src/config/FirebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBF8tvtspyNzoy4Ydu-ASH6KRh-3U0vzAw",
+    authDomain: "project-unicam-2fa57.firebaseapp.com",
+    databaseURL: "https://project-unicam-2fa57.firebaseio.com",
+    projectId: "project-unicam-2fa57",
+    storageBucket: "project-unicam-2fa57.appspot.com",
+    messagingSenderId: "78676571119"
+  };
+
+  export default config;
