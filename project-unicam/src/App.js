cd8a7958842ad24e0e914dcbc84eaab46c67e47b
NonnoFiorucci <matteoiommi@live.it>
2019-04-16 12:22:50 +0200

    CioBr
    Footer and Header added

--- project-unicam/src/App.js
+++ project-unicam/src/App.js
@@ -7,19 +7,23 @@ import {
 
 import SignUpPage from './components/pages/signup';
 import LandingPage from './components/pages/landing';
+import Header from './components/header';
+import Footer from './components/footer';
 
 
 
 const App = () => (
       <Router>
-   
+        <Header />
 
       
 
       <Route exact path={'/'} component={LandingPage} />
       <Route path={'/signup'} component={SignUpPage} />
      
-    
+      <Footer/>
+
+
   </Router>
   )
     
--- project-unicam/src/components/pages/landing/index.js
+++ project-unicam/src/components/pages/landing/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Landing(){
     return(
         <div>
-            cia
+            ciao, registrate stupetu
+            <Link to={'/signup'}> Pigia qui</Link>
+            
         </div>
     )
 }
