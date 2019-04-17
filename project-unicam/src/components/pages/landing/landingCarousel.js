a5745d0f514f7a1505a0fd60c57a9741a2f09ff7
NonnoFiorucci <matteoiommi@live.it>
2019-04-17 18:55:18 +0200

    linked images

--- project-unicam/src/components/pages/landing/landingCarousel.js
+++ project-unicam/src/components/pages/landing/landingCarousel.js
@@ -3,6 +3,7 @@ import { Carousel } from "react-bootstrap";
 import blog from '../../media/blog.jpg';
 import helpline from '../../media/helpline.jpg';
 import telegram from '../../media/telegram.png';
+import { Link } from 'react-router-dom';
 
 //eslint-disable-next-line
 import Style from '../../style.css';
@@ -12,21 +13,27 @@ const LandingCarousel = () => (
         <article className="left">
             <Carousel>
                 <Carousel.Item>
-                    <img src={telegram} alt="telegramSlide" width="800" height="400" />
+                <Link to={'/signin'}>
+                    <img src={telegram}  alt="telegramSlide" width="800" height="400" />
+                    </Link>
                     <Carousel.Caption>
                         <h3>TELEGRAM BOT</h3>
                         <p>Chatta con il nostro bot di telegram.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={blog} alt="blogSlide" width="800" height="400" />
+                <Link to={'/blog'}>
+                    <img src={blog}  alt="blogSlide" width="800" height="400" />
+                    </Link>
                     <Carousel.Caption>
                         <h3>BLOG</h3>
                         <p>Esprimiti liberamente nel nostro blog.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
+                <Link to={'/helpline'}>
                     <img src={helpline} alt="helplineSlide" width="800" height="400" />
+                    </Link>
                     <Carousel.Caption>
                         <h3>HELPLINE</h3>
                         <p>Non esitare a contattarci se lo ritieni necessario.</p>
