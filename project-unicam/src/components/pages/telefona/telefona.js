a6bea8ef1ab3a5bff5f7cc2aa02f851cb5336411
NonnoFiorucci <matteoiommi@live.it>
2019-04-19 11:32:10 +0200

    tel

--- project-unicam/src/components/pages/telefona/telefona.js
+++ project-unicam/src/components/pages/telefona/telefona.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Telefona() {
     return (
         <div>
             <h1>Telefona</h1>
+
+            <Button variant="outline-success" size="lg"><a href="tel:+393315909161">CHIAMA</a></Button>
         </div>
     );
 }
