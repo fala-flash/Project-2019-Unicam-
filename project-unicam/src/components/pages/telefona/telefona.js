d6f800cc47a6086c0aa057dfc4e3b7556b719dac
NonnoFiorucci <matteoiommi@live.it>
2019-04-19 11:48:54 +0200

    Update telefona

--- project-unicam/src/components/pages/telefona/telefona.js
+++ project-unicam/src/components/pages/telefona/telefona.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
+import Alert from 'react-bootstrap/Alert';
 
 function Telefona() {
     return (
         <div>
             <h1>Telefona</h1>
+      
 
-            <Button variant="outline-success" size="lg"><a href="tel:+393315909161">CHIAMA</a></Button>
+  <Alert variant="success">
+  <Alert.Heading>Chiama!</Alert.Heading>
+  <p>
+  Per ogni emergenza, noi siamo disponibili e volenterosi di ascoltarti. Chiamaci! </p>
+  <p>
+  Il nostro addetto alle chiamate: Marco.
+
+  </p>
+  <hr />
+  <p className="mb-0">
+  <Button variant="outline-success" size="lg"><a  text="white" href="tel:+393315909161">CHIAMA</a></Button>
+  </p>
+</Alert>;
         </div>
     );
 }
