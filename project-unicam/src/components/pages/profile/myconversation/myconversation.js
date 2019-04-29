98cac8ad803ef68cc4ffc99a3a24b0ca497bb041
fala-flash <44972556+fala-flash@users.noreply.github.com>
2019-04-29 11:43:58 +0200

    Update myconversation.js

--- project-unicam/src/components/pages/profile/myconversation/myconversation.js
+++ project-unicam/src/components/pages/profile/myconversation/myconversation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Popover, OverlayTrigger } from "react-bootstrap";
 import { FaAngleLeft } from "react-icons/fa";
 
 function MyConversation() {
@@ -8,7 +8,24 @@ function MyConversation() {
       <Button variant="info" href="/profile">
         <FaAngleLeft />
       </Button>
-      <h1>Guarda che belle chat!</h1>
+
+      <br />
+      <br />
+
+      <OverlayTrigger
+        trigger="click"
+        key="bottom"
+        placement="bottom"
+        overlay={
+          <Popover id={`popover-positioned-bottom`} title={`Conversazione 1`}>
+            <h1>ciao</h1>
+            <h1>ciao</h1>
+            <h1>ciao</h1>
+          </Popover>
+        }
+      >
+        <Button variant="outline-info">Conversazione 1</Button>
+      </OverlayTrigger>
     </div>
   );
 }
