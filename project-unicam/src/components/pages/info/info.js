abb294939d51f23389abd278dd2e5d3d5b3f155b
NonnoFiorucci <matteoiommi@live.it>
2019-04-30 12:09:19 +0200

    up

--- project-unicam/src/components/pages/info/info.js
+++ project-unicam/src/components/pages/info/info.js
@@ -13,11 +13,10 @@ function Info() {
                 <Card bg="info" text="white" style={{ fontWeight:'bold' }}>
                     <Card.Header>Stop! Bullying</Card.Header>
                     <Card.Body>
-                        <ul style={{listStyleType: 'none', textAlign: 'left'}}>
-                            <li>v 1.0.0</li>
-                            <li>Web, Android, iOS</li>
-                            <li>Sviluppato da: <a href="https://www.unicam.it/" style={{color:'white', textDecoration:'underline', fontWeight:'bold', textDecorationColor:'white'}}> Unicam</a></li>
-                        </ul>
+                            <p>v 1.0.0</p>
+                            <p>Web, Android, iOS</p>
+                            <p>Sviluppato da: <a href="https://www.unicam.it/" style={{color:'white', textDecoration:'underline', fontWeight:'bold', textDecorationColor:'white'}}> Unicam</a></p>
+
                     </Card.Body>
                 </Card>
             </div>
@@ -28,12 +27,11 @@ function Info() {
                 <Card bg="info" text="white" style={{ fontWeight:'bold' }}>
                     <Card.Header>Seguici</Card.Header>
                     <Card.Body>
-                        <ul style={{listStyleType: 'none', textAlign: 'left'}}>
-                            <li><a href="https://www.instagram.com/?hl=it" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Instagram </a><FaInstagram/></li>
-                            <li><a href="https://www.facebook.com/" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Facebook </a><FaFacebook/></li>
-                            <li><a href="https://www.youtube.com/" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Youtube </a><FaYoutube/></li>
-                            <li><a href="https://twitter.com/" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Twitter </a><FaTwitter/></li>
-                        </ul>         
+    
+                            <p><a href="https://www.instagram.com/?hl=it" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Instagram </a><FaInstagram/></p>
+                            <p><a href="https://www.facebook.com/" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Facebook </a><FaFacebook/></p>
+                            <p><a href="https://www.youtube.com/" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Youtube </a><FaYoutube/></p>
+                            <p><a href="https://twitter.com/" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>Twitter </a><FaTwitter/></p>        
                     </Card.Body>
                 </Card>
             </div>
@@ -44,10 +42,9 @@ function Info() {
                 <Card bg="info" text="white" style={{ fontWeight:'bold' }}>
                     <Card.Header>Contattaci</Card.Header>
                     <Card.Body>
-                        <ul style={{listStyleType: 'none', textAlign: 'left'}}>
-                            <li><a href="tel:0123456789" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>0123456789 </a><FaPhone/></li>
-                            <li><a href="mailto:project.unicam@gmail.com" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>project.unicam@gmail.com </a><MdEmail/></li>
-                        </ul>
+                            <p><a href="tel:0123456789" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>0123456789 </a><FaPhone/></p>
+                            <p><a href="mailto:project.unicam@gmail.com" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>project.unicam@gmail.com </a><MdEmail/></p>
+
                     </Card.Body>
                 </Card>
             </div>
