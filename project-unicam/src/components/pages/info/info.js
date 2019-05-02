0f19a44b5c4367f7fcc9d248185f6dbaef4c1700
fala-flash <44972556+fala-flash@users.noreply.github.com>
2019-05-02 10:34:59 +0200

    infocard

--- project-unicam/src/components/pages/info/info.js
+++ project-unicam/src/components/pages/info/info.js
@@ -10,7 +10,7 @@ function Info() {
     return (
         <div>
             <div className="infoCard" style={{display:'flex', justifyContent:'center'}}>
-                <Card bg="info" text="white" style={{ fontWeight:'bold' }}>
+                <Card bg="info" text="white" style={{ fontWeight:'bold', width: '18rem' }}>
                     <Card.Header>Stop! Bullying</Card.Header>
                     <Card.Body>
                             <p>v 1.0.0</p>
@@ -24,7 +24,7 @@ function Info() {
             <br></br>
 
             <div className="infoCard" style={{display:'flex', justifyContent:'center'}}>
-                <Card bg="info" text="white" style={{ fontWeight:'bold' }}>
+                <Card bg="info" text="white" style={{ fontWeight:'bold', width: '18rem' }}>
                     <Card.Header>Seguici</Card.Header>
                     <Card.Body>
     
@@ -39,7 +39,7 @@ function Info() {
             <br></br>
             
             <div className="infoCard" style={{display:'flex', justifyContent:'center'}}>
-                <Card bg="info" text="white" style={{ fontWeight:'bold' }}>
+                <Card bg="info" text="white" style={{ fontWeight:'bold', width: '18rem' }}>
                     <Card.Header>Contattaci</Card.Header>
                     <Card.Body>
                             <p><a href="tel:0123456789" style={{color:'white', fontWeight:'bold', textDecoration:'underline', textDecorationColor:'white'}}>0123456789 </a><FaPhone/></p>
