e58a2339cdf571b2a7cf5e01e68c2e82fe4b9e6d
fala-flash <44972556+fala-flash@users.noreply.github.com>
2019-05-07 09:30:48 +0200

    Revert "update"
    This reverts commit f7c933f43bf093e58a711886f7483f02c5ca00e3.

--- project-unicam/src/components/header/header.js
+++ project-unicam/src/components/header/header.js
@@ -11,14 +11,14 @@ class Header extends Component {
             <div>
                 {this.props.authenticated
                     ?
-                    <Navbar className="navbarTesto" fixed="top" bg="info" variant="light" expand="light">
+                    <Navbar fixed="top" bg="info" variant="light" expand="light">
                         <Navbar.Brand  className="navbarbrand"style={{color:'white'}} href="/">Stop! Bullying</Navbar.Brand>
                         <Navbar.Toggle style={{backgroundColor:'white'}}aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
-                                <Nav.Link style={{color:'white'}} href="/faq">FAQ</Nav.Link>
-                                <Nav.Link style={{color:'white'}} href="/info">Info</Nav.Link>
-                                <Nav.Link style={{color:'white'}} href="/logout">Logout</Nav.Link>
+                                <Nav.Link style={{color:'white', fontWeight:'bold'}} href="/faq">FAQ</Nav.Link>
+                                <Nav.Link style={{color:'white', fontWeight:'bold'}} href="/info">Info</Nav.Link>
+                                <Nav.Link style={{color:'white', fontWeight:'bold'}} href="/logout">Logout</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
